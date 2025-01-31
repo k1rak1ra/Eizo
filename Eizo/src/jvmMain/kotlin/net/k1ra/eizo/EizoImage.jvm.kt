0298f9dc56f4b62f95d0b31df2bd852c420958d6
Ollie Gaus <k1ra@k1ra.net>
2025-01-30 21:49:23 -0500

    Added missing jvm impl

---
+++ Eizo/src/jvmMain/kotlin/net/k1ra/eizo/EizoImage.jvm.kt
@@ -0,0 +1,17 @@
+package net.k1ra.eizo
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+
+actual val ioDispatcher: CoroutineDispatcher
+    get() = Dispatchers.IO
+
+actual fun cacheExecutor(
+    checkLogic: suspend () -> Boolean,
+    networkFetchLogic: () -> Unit
+) {
+    if (!runBlocking { checkLogic.invoke() }) {
+        networkFetchLogic.invoke()
+    }
+}
